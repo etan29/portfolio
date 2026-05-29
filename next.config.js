2af5945595a103d420862595eca8900639a954e1
etan29 <emilytan2992@gmail.com>
2026-05-28 22:38:28 -0700

    Fix GitHub Pages workflow YAML error and set basePath for project site.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
+const repo = "portfolio";
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   images: {
     loader: "akamai",
-    path: "",
+    path: isGithubPages ? `/${repo}/` : "/",
     unoptimized: true,
   },
-  basePath: "",
-  assetPrefix: "",
+  basePath: isGithubPages ? `/${repo}` : "",
+  assetPrefix: isGithubPages ? `/${repo}` : "",
 };
 
 module.exports = nextConfig;
